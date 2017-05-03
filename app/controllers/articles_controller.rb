bd4f7738d5627c00f7ae24815cff76e67b6fbb6e
Nabin <nabin@studiomatrix.com.np>
2017-05-03 16:39:19 +0545

    Extract method and remove redundancy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+	before_action :find_article, only: [:edit, :update, :show, :destroy]
 	def index
 		@articles = Article.all
 	end
@@ -17,12 +18,10 @@ class ArticlesController < ApplicationController
 		end
 	end
 
-	def edit
-		@article = Article.find(params[:id])		
+	def edit		
 	end
 
 	def update
-		@article = Article.find(params[:id])
 		if @article.update(article_params)
 			flash[:notice] = "Article was successfully created"
 			redirect_to article_path(@article)
@@ -32,11 +31,9 @@ class ArticlesController < ApplicationController
 	end
 
 	def show
-		@article = Article.find(params[:id])
 	end 
 
 	def destroy
-		@article = Article.find(params[:id])
 		@article.destroy
 		flash[:notice] = "Article was successfully deleted"
 		redirect_to articles_path
@@ -45,5 +42,9 @@ class ArticlesController < ApplicationController
 	private
 		def article_params
 			params.require(:article).permit(:title, :description)
+		end
+
+		def find_article
+			@article = Article.find(params[:id])
 		end
 end 
