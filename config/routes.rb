27435ee5de4f8382685f39a845f3b732ec6fd0bb
Nabin <nabin@studiomatrix.com.np>
2017-05-03 14:36:43 +0545

    Create new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ AlphaBlog::Application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
 
+  resources :articles
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
