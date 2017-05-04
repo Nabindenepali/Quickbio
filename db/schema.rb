a797a0094848bffdf4d969d5f861933d2682ab37
Nabin <nabin@studiomatrix.com.np>
2017-05-04 15:06:51 +0545

    create users table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503064016) do
+ActiveRecord::Schema.define(version: 20170504091534) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users_tables", force: true do |t|
   end
 
 end
