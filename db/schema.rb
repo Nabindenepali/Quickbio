f7fb5563074ef994d34fd48ad343c7a93594e2bc
Nabin <nabin@studiomatrix.com.np>
2017-05-05 17:33:54 +0545

    Add category model and test for categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170505101532) do
+ActiveRecord::Schema.define(version: 20170505113539) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20170505101532) do
     t.integer  "user_id"
   end
 
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", force: true do |t|
     t.string   "username"
     t.string   "email"
@@ -28,9 +34,6 @@ ActiveRecord::Schema.define(version: 20170505101532) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.boolean  "admin",           default: false
-  end
-
-  create_table "users_tables", force: true do |t|
   end
 
 end
