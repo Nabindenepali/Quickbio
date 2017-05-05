72537326bda90ebe3746f6e0591059146895fdb5
Nabin <nabin@studiomatrix.com.np>
2017-05-05 14:39:42 +0545

    Write user authentication methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:edit, :update, :show]
 	def index
 		@users = User.paginate(page: params[:page], per_page: 5)
 	end
@@ -17,12 +18,10 @@ class UsersController < ApplicationController
 		end
 	end
 
-	def edit
-		@user = User.find(params[:id])
+	def edit		
 	end
 
-	def update
-		@user = User.find(params[:id])
+	def update		
 		if @user.update(user_params)
 			flash[:success] = "Your account was successfully updated"
 			redirect_to articles_path
@@ -31,13 +30,16 @@ class UsersController < ApplicationController
 		end
 	end
 
-	def show
-		@user = User.find(params[:id])
+	def show		
 		@user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
 	end
 
 	private
 	def user_params
 		params.require(:user).permit(:username, :email, :password, :password_confirmation)
+	end
+
+	def set_user 
+		@user = User.find(params[:id])
 	end
 end  
