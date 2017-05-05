3aa8f6064290d06035e5070601e49adf5ef4b2aa
Nabin <nabin@studiomatrix.com.np>
2017-05-05 16:36:35 +0545

    Add admin functionalities

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504110610) do
+ActiveRecord::Schema.define(version: 20170505101532) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170504110610) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
   create_table "users_tables", force: true do |t|
